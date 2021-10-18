12fb9c6457ed6207526d64b657fd111283cba655
Dartorr <linevich.tcheslav@ya.ru>
2021-10-18 17:10:40 +0300

    Добавил 4 и 5 лр

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleApp4
+{
+    public class MN
+    {
+        
+        public List<int> mn = new List<int>() { };
+        public readonly Date date = new Date();
+        public readonly Owner owner = new Owner();
+
+
+        public class Owner
+        {
+            public int ID { get; }
+            public string Author { get; }
+            public string Organization { get; }
+
+            public Owner()
+            {
+                this.ID = 123456789;
+                this.Author = "Л.Ч.С.";
+                this.Organization = "БГТУ";
+            }
+        }
+        public class Date
+        {
+            public DateTime Time { get; }
+
+            public Date()
+            {
+                Time = DateTime.Now;
+            }
+        }
+
+
+        public MN(int[] a) //Конструктор основного MN
+        {
+            List<int> temp = new List<int>() { };
+            temp.AddRange(a);
+            mn = temp;
+            owner = new Owner();
+            date = new Date();
+        }
+
+        public MN() //конструктор буферного MN
+        {
+            
+        }
+        public static bool operator >>(MN mn, int s) //удаление элемента
+        {
+            bool a = mn.mn.Remove(s);
+            foreach (int c in mn.mn) Console.Write($"{c}\t");
+            Console.Write("\n");
+            return a;
+        }
+
+        public int add ( int s) //метод добавления элемента
+        {
+
+            if (this.mn.IndexOf(s) == -1)
+                this.mn.Add(s);
+            
+            return 0;
+        }
+        public static int operator <<(MN mn, int s) //добавление элемента
+        {
+
+            if (mn.mn.IndexOf(s) == -1)
+                mn.mn.Add(s);
+            foreach (int c in mn.mn) Console.Write($"{c}\t");
+            Console.Write("\n");
+            return 0;
+        }
+        public static bool operator >(MN mn, MN pod) //проверка на подмножество
+        {
+            bool flag = true;
+            {
+                foreach (int c in pod.mn)
+                    if (mn.mn.IndexOf(c) == -1) flag = false;
+            }
+            
+            return flag;
+        }
+        public static bool operator <(MN mn, MN mn2)
+        {
+            return false;
+        }
+
+        public static bool operator !=(MN mn, MN mn2) //проверка множеств на неравенство
+        {
+            bool flag = true;
+            {
+                foreach (int c in mn2.mn)
+                    if (mn.mn.IndexOf(c) == -1) flag = false;
+            }
+            return flag;
+        }
+        public static bool operator ==(MN mn, MN mn2)
+        {
+            return false;
+        }
+
+        public static MN operator %(MN mn, MN mn2) //проверка на пересечение
+        {
+            MN per = new MN();
+            foreach (int c in mn.mn)
+            {
+                if (mn2.mn.IndexOf(c) != -1)
+                {
+                    per.add(c);
+                }
+            }
+            return per;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 1, 2, 3 };
+            int[] b = new int[] { 2, 3, 4 };
+
+            MN oleg = new MN(a);
+            MN oleg2 = new MN(b);
+
+            MN pod = new MN(new int[] { 1 });
+
+            foreach (int c in oleg.mn) Console.Write($"{c}\t"); //вывод множества
+            Console.Write("\n");
+            foreach (int c in oleg2.mn) Console.Write($"{c}\t"); //вывод множества
+            Console.Write("\n");
+            Console.Write("\n");
+            {
+                Console.WriteLine(oleg >> (2));//удаление. True, если удалено успешно
+                Console.WriteLine((oleg > pod));//проверка. True, если является подмножеством
+                Console.WriteLine(oleg << (2));//добавление. 0, если операция прошла успешно
+                Console.WriteLine(oleg != oleg2);//равенство. False, если множества не равны
+            }
+
+            MN mn = oleg % oleg2; //переменная с пересечением
+
+            foreach (int c in mn.mn) Console.Write($"{c}\t"); //вывод пересечения
+            Console.Write("\n");
+
+            mn.sort(); //сортировка
+            foreach (int c in mn.mn) Console.Write($"{c}\t");
+            Console.Write("\n");
+            Console.WriteLine(mn.smoller()); //наименьшее число
+
+            Console.WriteLine($"{ oleg.owner.ID}\t{ oleg.owner.Author}\t{ oleg.owner.Organization}\t{oleg.date.Time}");//Данные о владельце и дата
+            Console.WriteLine($"{mn.minus()}\t{mn.sum()}\t{mn.kol()}\t");//макс-мин, сумма всех элементов, количество элементов
+        }
+        
+    }
+    public static class MethodExtension //Методы расширения
+    {
+        public static string smoller(this MN mn)
+        {
+            int min = 1000000;
+            foreach (int c in mn.mn)
+            {
+                if (c < min) min = c;
+            }
+            return min.ToString();
+        }
+
+        public static MN sort(this MN mn)
+        {
+            
+            mn.mn.Sort();
+            
+            return mn;
+        }
+        public static int sum(this MN mn)
+        {
+            int sum = 0;
+            foreach (int c in mn.mn) sum += c;
+            return sum;
+        }
+
+        public static int minus(this MN mn)
+        {
+            int min = 10000;
+            int max = 0;
+            foreach (int c in mn.mn)
+            {
+                if (c < min) min = c;
+                if (max < c) max = c;
+            }
+            return (max - min);
+        }
+
+        public static int kol(this MN mn)
+        {
+            int k = 0;
+            foreach (int c in mn.mn)
+            {
+                k++;
+            }
+            return k;
+        }
+    }
+}
---
+++ LW5/LW5/Program.cs
@@ -0,0 +1,220 @@
+﻿using System;
+
+
+/*
+5) Написать демонстрационную программу, в которой создаются объекты
+различных классов. Поработать с объектами через ссылки на абстрактные
+классы и интерфейсы. В этом случае для идентификации типов объектов
+использовать операторы is или as.
+7) Создайте дополнительный класс Printer c полиморфным методом
+IAmPrinting( SomeAbstractClassorInterface someobj). Формальным
+параметром метода должна быть ссылка на абстрактный класс или наиболее
+общий интерфейс в вашей иерархии классов. В методе iIAmPrinting
+определите тип объекта и вызовите ToString(). В демонстрационной
+программе создайте массив, содержащий ссылки на разнотипные объекты
+ваших классов по иерархии, а также объект класса Printer и последовательно
+вызовите его метод IAmPrinting со всеми ссылками в качестве аргументов.
+ */
+namespace LW5
+{
+   
+    class Program
+    {
+        public  class Printer
+        {
+            public  int IAmPrinting(ref Stuff oleg)
+            {
+                Console.WriteLine(oleg.GetType());
+                Console.WriteLine(oleg.ToString());
+                return 0;
+            }
+            
+        }
+        interface IDunno
+        {
+            public int SomeCommonMethod()
+            {
+                Console.WriteLine("Interface Method");
+                return 2 + 2;
+            }
+        }
+        public abstract class Stuff
+        {
+            public  int Cost;
+            public  int Weight;
+
+            
+            public virtual int SomeCommonMethod()
+            {
+                Console.WriteLine("Abstract Class Method");
+                return 2 + 2;
+            }
+
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+            public override string ToString()
+            {
+                return base.ToString();
+            }
+            public virtual int ShowCost()
+            {
+                return 0;
+            }
+
+        }
+
+        public abstract class Tech: Stuff 
+        {
+            public  string Model;
+            public  int Power_consumption;
+            public  int Complexity;
+            public override int ShowCost()
+            {
+                Console.WriteLine(this.Model);
+                return this.Cost;
+            }
+        }
+
+        public class Print_er : Tech, IDunno
+        {
+            public int Print_Speed;
+
+            public Print_er(int cost, int wd, string mdl, int pc, int cmlx, int spd)
+            {
+                Cost = cost;
+                Weight = wd;
+                Model = mdl;
+                Power_consumption = pc;
+                Complexity = cmlx;
+                Print_Speed = spd;
+            }
+            public override string ToString()
+            {
+                Console.WriteLine($"{this.Cost}\t{this.Weight}\t{this.Model}\t{this.Power_consumption}\t{this.Complexity}\t{this.Print_Speed}\t");
+                return "0leg";
+            }
+
+            int IDunno.SomeCommonMethod()
+            {
+                Console.WriteLine("Overriden Interface Method ");
+                return 4;
+            }
+
+            public override int SomeCommonMethod()
+            {
+                Console.WriteLine("Overriden Abstract Class Method ");
+                return 5;
+            }
+
+        }
+
+        public sealed class Grafic_Card
+        {
+            public string Model;
+        }
+        public class Personal_Computer : Tech
+        {
+            public int Performance;
+            public Grafic_Card Card;
+
+            public Personal_Computer(int cost, int wd, string mdl, int pc, int cmlx, int prf, string gc_model)
+            {
+                Cost = cost;
+                Weight = wd;
+                Model = mdl;
+                Power_consumption = pc;
+                Complexity = cmlx;
+                Performance = prf;
+
+                Card = new Grafic_Card
+                {
+                    Model = gc_model
+                };
+            }
+            public override string ToString()
+            {
+                Console.WriteLine($"{this.Cost}\t{this.Weight}\t{this.Model}\t{this.Power_consumption}\t{this.Complexity}\t{this.Performance}\t{this.Card.Model}");
+                return "0";
+            }
+        }
+
+        public class Scaner : Tech
+        {
+            public int Scan_Speed;
+
+            public Scaner(int cost, int wd, string mdl, int pc, int cmlx, int spd)
+            {
+                Cost = cost;
+                Weight = wd;
+                Model = mdl;
+                Power_consumption = pc;
+                Complexity = cmlx;
+                Scan_Speed = spd;
+            }
+            public override string ToString()
+            {
+                Console.WriteLine($"{this.Cost}\t{this.Weight}\t{this.Model}\t{this.Power_consumption}\t{this.Complexity}\t{this.Scan_Speed}\t");
+                return "0";
+            }
+        }
+
+        public class Tablet : Tech
+        {
+            public int Performance;
+
+            public Tablet(int cost, int wd, string mdl, int pc, int cmlx, int prf)
+            {
+                Cost = cost;
+                Weight = wd;
+                Model = mdl;
+                Power_consumption = pc;
+                Complexity = cmlx;
+                Performance = prf;
+            }
+            public override string ToString()
+            {
+                Console.WriteLine($"{this.Cost}\t{this.Weight}\t{this.Model}\t{this.Power_consumption}\t{this.Complexity}\t{this.Performance}\t");
+                return "0";
+            }
+        }
+
+        static void Main()
+        {
+            Tablet tablet = new Tablet(50, 3, "3 model", 300, 3, 500);
+            Scaner scan = new Scaner(30, 5, "0 model", 150, 5, 400);
+            Personal_Computer PC = new Personal_Computer(50, 3, "haha model", 300, 10, 2000,"mx250");
+            Print_er print = new Print_er(50, 3, "3 model", 300, 3, 500);
+
+            tablet.ToString();
+            scan.ToString();
+            PC.ToString();
+            print.ToString();
+
+            Console.WriteLine(tablet is Tablet);
+            Console.WriteLine(tablet is Personal_Computer);
+            Console.WriteLine(tablet is Tech);
+            Console.WriteLine(tablet is Stuff);
+
+            ref Tablet tablet_ref = ref tablet;
+            ref Scaner scaner_ref = ref scan;
+            ref Personal_Computer PC_ref = ref PC;
+            ref Print_er print_ref = ref print;
+
+            Stuff[] st = { tablet_ref, scaner_ref, PC_ref, print_ref };
+
+            Printer printer = new Printer();
+
+            printer.IAmPrinting(ref st[0]);
+            printer.IAmPrinting(ref st[1]);
+            printer.IAmPrinting(ref st[2]);
+            printer.IAmPrinting(ref st[3]);
+        }   
+    }
+}
